24507acb095102f81a00b0f13ec8be14f82f400b
IvanETU001597 <ivantahiana@gmail.com>
2026-02-02 08:56:55 +0300

    feat: add navbar

--- src/app/page/admin/dashboard/dashboard.ts
+++ src/app/page/admin/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
+import { Navbar } from './../../../navbar/navbar';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
