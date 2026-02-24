4176ac052fe5eff7b2cb0b85b10e63d19d790bc1
IvanETU001597 <ivantahiana@gmail.com>
2026-02-24 10:08:51 +0300

    feat: client command

---
+++ src/app/services/order.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+  private API = `${environment.api}`;
+
+  constructor(private http: HttpClient) {}
+
+  createOrder(order: any, shopId: string): Observable<any> {
+    return this.http.post(`${this.API}/orders`, { ...order, shopId });
+  }
+
+  getClients(): Observable<any> {
+    return this.http.get(`${this.API}/client/clients`);
+  }
+
+  getServicesByShop(shopId: string): Observable<any> {
+    return this.http.get(`${this.API}/shop/service/services/${shopId}`);
+  }
+}
