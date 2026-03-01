f997111495bef6a6ae10fed8e08475016dbdb22c
IvanETU001597 <ivantahiana@gmail>
2026-03-01 09:12:03 +0300

    feat: display stat number

--- src/app/page/admin/dashboard/dashboard.component.ts
+++ src/app/page/admin/dashboard/dashboard.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TauxRevenuComponent } from "./taux-revenu/taux-revenu.component";
 import { CommonModule } from '@angular/common';
 import { TauxRevenuMensuelComponent } from "./taux-revenu-mensuel/taux-revenu-mensuel.component";
+import { first, firstValueFrom } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+type Stats = {
+  nbrClient?: number;
+  nbrService?: number;
+  nbrShop?: number;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +18,48 @@ import { TauxRevenuMensuelComponent } from "./taux-revenu-mensuel/taux-revenu-me
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
+
 export class DashboardComponent {
+  stats: Stats = {};
+  isLoading = false;
+  errorMessage = '';
+  
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadStats();
+  }
+
+  async loadStats() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      this.errorMessage = 'Vous devez vous connecter.';
+      return;
+    }
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    try {
+      const response: Stats = await firstValueFrom(
+        this.http.get<Stats>(`${environment.api}/admin/stats`, {
+          headers: new HttpHeaders({
+            Authorization: `Bearer ${token}`,
+          }),
+        })
+      )
+      console.log('Stats response:', response);
+      this.stats = response;
+      this.isLoading = false;
+    } catch (error: any){
+      this.errorMessage = error?.error?.message || error?.error?.error || 'Erreur chargement ';
+      this.stats = {};
+    }
+  }
+  
+
+
+
+
 
 }
